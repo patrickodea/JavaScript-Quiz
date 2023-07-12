d1a04e9e4c25aa9d22cfe592e112fdadfe5c24ed
Patrick O'Dea <po39dea@gmail.com>
2023-07-12 15:09:57 -0600

    Timer Set Up

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,28 @@
+var startQuiz = document.querySelector("#button")
 var timer = document.querySelector(".timer");
-
-
 var secondsLeft = 75;
 
-function startTime(){
-    var timerInterval = setInterval(function(){
-    secondsLeft--;
-    time.textContent = "Time: " + secondsLeft;
+    function setTime(){ 
 
-    if(secondsLeft === 75){
-        clearInterval(timerInterval);
-    }
-}, 1000);
+    var timeEnd = setInterval(function() {
+
+        secondsLeft--;
+        timer.textContent = "Time Left: " + secondsLeft;
+
+        if(secondsLeft === 0){
+            clearInterval(timeEnd);
+            sendMessage();
+        }
+    }, 1000);
+ }
+
+
+
+
+function sendMessage(){
+    timer.textContent = " ";
+    var timeUp = document.createElement ("h1");
+    h1.setAttribute("Times Up!");
+
 }
+setTime();
