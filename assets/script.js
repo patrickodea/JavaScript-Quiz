6e8890a909683d16991449d19ac9f1b8509c574c
Patrick O'Dea <po39dea@gmail.com>
2023-10-18 00:42:28 -0600

    First question can be displayed

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,8 @@ var timer = document.querySelector(".timer");
 var quiz = document.querySelector("#quiz");
 var secondsLeft = 76;
 
+
+
     function setTime(event){ 
         event.preventDefault(setTime);
     var timeEnd = setInterval(function() {
@@ -11,27 +13,43 @@ var secondsLeft = 76;
 
         if(secondsLeft === 0){
             clearInterval(timeEnd);
+            var quizSection = document.getElementById("quiz");
+            quizSection.style.display = "none";
             sendMessage();
         }
     }, 1000);
- }
-
-    function quizQuestions(){
-        var quizQuestions = document.getElementById("#quiz")
- }   
+}
 
 
 
+function quizQuestionOne(event) {
+    event.preventDefault();
+    var homepageSection = document.querySelector("section");
+    var quizSection = document.createElement("section");
+    quizSection.innerHTML = `
+    <h2>Question 1</h2>
+    <p>Inside which HTML Element do we put the JS?</p>
+    <button>script</button>
+    <button>javascript</button>
+    <button>js</button>
+    <button>scripting</button>
+    `;
+    homepageSection.style.display = "none";
+    document.body.appendChild(quizSection);
+}
 
 
- buttonClick.addEventListener("click", setTime);
- buttonClick.addEventListener("click", quizQuestions);
+
+
+buttonClick.addEventListener("click", setTime);
+buttonClick.addEventListener("click", quizQuestionOne);
 
 
 
 function sendMessage(){
     timer.textContent = " ";
     var timeEnd = document.createElement ("h1");
-    h1.setAttribute("Times Up!");
+    timeEnd.textContent = "Times Up";
+    document.body.appendChild(timeEnd);
 
 }
